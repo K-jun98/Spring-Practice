f8743471e21dfa19cf1dd5b8f07af5a2c994fa1a
jun <alap_u@naver.com>
2023-06-30 19:21:10 +0900

    Testing the Web Layer

---
+++ web-layer-test/src/test/java/com/spring/weblayertest/WebLayerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.weblayertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLayerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
