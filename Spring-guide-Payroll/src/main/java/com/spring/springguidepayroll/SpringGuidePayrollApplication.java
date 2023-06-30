27335dca99c701b23750ad86090e14689846153d
jun <alap_u@naver.com>
2023-06-30 15:07:06 +0900

    스프링 가이드 프로젝트 초기화

---
+++ Spring-guide-Payroll/src/main/java/com/spring/springguidepayroll/SpringGuidePayrollApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springguidepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuidePayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuidePayrollApplication.class, args);
+	}
+
+}
---
+++ Spring-guide-Payroll/src/test/java/com/spring/springguidepayroll/SpringGuidePayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springguidepayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGuidePayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
